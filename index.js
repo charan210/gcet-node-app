5dc7eca84c52810384cf7329c77e120e5be040e5
charan210 <reddycharan2580@gmail.com>
2025-06-03 10:48:50 +0530

    u

--- index.js
+++ index.js
@@ -1,63 +1,24 @@
-import express from "express";
-import cors from "cors";
+import express from 'express'
 const app = express();
-app.listen(8080, () => {
-  console.log("Server Started");
+app.listen(8081, () => {
+console.log("Server Started");
 });
-app.use(cors());
+
 app.get("/", (req, res) => {
-  return res.send("Good Morning");
+    return res.send("good morning")
 });
 
-app.get("/greet", (req, res) => {
-  res.send("Greetings");
-});
+app.get("/greet",(req, res) =>{res.send("greetings")});
 
-app.get("/name", (req, res) => {
-  res.send("Praveen");
-});
+app.get("/name",(req, res)=>{res.send("vinaykumar")});
 
-app.get("/weather", (req, res) => {
-  res.send("31degree");
-});
+app.get("/weather",(req, res)=>{res.send("24")})
 
-app.get("/products", (req, res) => {
-  const products = [
+app.get("/products",(req, res) => {
+    const products = [
     { name: "Product 1", price: 34 },
     { name: "Product 2", price: 64 },
     { name: "Product 3", price: 45 },
   ];
   res.json(products);
 });
-
-// import express from "express";
-// import cors from "cors";
-// const app = express();
-// app.listen(8080, () => {
-//   console.log("Server Started on port 8080");
-// });
-
-// app.use(express.json());
-// app.use(cors());
-// const users = [];
-
-// app.get("/", (req, res) => {
-//   return res.json(users);
-// });
-
-// app.post("/register", (req, res) => {
-//   const { name, email, pass } = req.body;
-//   users.push({ name, email, pass });
-//   return res.json(users);
-// });
-
-// app.post("/login", (req, res) => {
-//   const { email, pass } = req.body;
-//   const found = users.find(
-//     (value) => value.email === email && value.pass === pass
-//   );
-//   if (!found) {
-//     res.json({message:"User not found"})
-//   }
-//   res.json({...found,token:"123"});
-// });
